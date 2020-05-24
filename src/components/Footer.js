ac0c80c176300758515cb5f999f0b12a9c29b2ff
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2020-05-24 23:21:36 +1000

    link to source

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ export default class Footer extends Component {
           </a>
         </div>
         <div>
-          <a href="" target="_blank" rel="noopener noreferrer">Source</a>
+          <a href="https://github.com/calvinbui/calvin.me" target="_blank" rel="noopener noreferrer">Source</a>
           <a href="/rss.xml" target="_blank" rel="noopener noreferrer">RSS</a>
         </div>
       </footer>
