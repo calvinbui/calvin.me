06147a6dc6674f43a24a51ad5304078a918adcf5
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2020-08-24 04:14:10 +1000

    my wfh setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,6 +71,16 @@ module.exports = {
       options: {
         plugins: [
           {
+            resolve: `gatsby-remark-table-of-contents`,
+            options: {
+              exclude: "Table of Contents",
+              tight: false,
+              fromHeading: 1,
+              toHeading: 6,
+              className: "table-of-contents",
+            },
+          },
+          {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 850,
