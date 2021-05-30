d6aaf30369e12cc15b133f829789c217756cecbd
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2021-05-30 19:09:42 +1000

    add analytics to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,6 +10,7 @@ export default class Footer extends Component {
           </a>
         </div>
         <div>
+          <a href="https://umami.calvin.me/share/AkrFoOXj/calvin.me" target="_blank" rel="noopener noreferrer">Analytics</a>
           <a href="https://github.com/calvinbui/calvin.me" target="_blank" rel="noopener noreferrer">Source</a>
           <a href="/rss.xml" target="_blank" rel="noopener noreferrer">RSS</a>
         </div>
