06fdb8fcef58921a865bbff5b1ec0ed00c69b8c8
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2021-09-28 21:21:34 +1000

    fix edit button

--- src/utils/global.js
+++ src/utils/global.js
@@ -5,8 +5,9 @@ import config from '../../data/SiteConfig'
 const formatDate = date => moment.utc(date).format(config.dateFormat)
 
 const editOnGithub = post => {
+  const year = moment.utc(post.date).format('YYYY')
   const date = moment.utc(post.date).format(config.dateFromFormat)
-  return urljoin(config.repo, 'edit/master/posts', `${date}-${post.id}/index.md`)
+  return urljoin(config.repo, 'edit/master/posts', `${year}`, `${date}-${post.id}/index.md`)
 }
 
 export { formatDate, editOnGithub }
