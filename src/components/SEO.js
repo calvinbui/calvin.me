e4beba093cc8e2e2f911f91417a52af538b7208b
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2022-12-16 15:36:13 +1100

    fix lint

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { Helmet } from 'react-helmet'
-import { getSrc } from "gatsby-plugin-image"
 import urljoin from 'url-join'
 import config from '../../data/SiteConfig'
 
