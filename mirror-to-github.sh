a7217ead852fd3233334fdd9c9f295779fee5370
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2023-10-24 04:19:13 +1100

    mirror using gitea

--- mirror-to-github.sh
+++ mirror-to-github.sh
@@ -2,12 +2,9 @@
 
 set -euo pipefail
 
-username="calvinbui"
-token=$GITHUB_PAT
-repo="calvinbui/calvin.me"
-
-echo "--- Add safe directory"
-git config --global --add safe.directory '*'
+username=$USER_NAME
+token=$GH_PAT
+repo=$REPOSITORY
 
 echo "--- Add GitHub origin"
 git remote add github "https://${username}:${token}@github.com/${repo}.git"
