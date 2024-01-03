ad88d8efbe263e62df843352daa126d9b25435ac
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2024-01-04 08:25:09 +1100

    fetch and get all history

--- mirror-to-github.sh
+++ mirror-to-github.sh
@@ -9,5 +9,8 @@ repo=$REPOSITORY
 echo "--- Add GitHub origin"
 git remote add github "https://${username}:${token}@github.com/${repo}.git"
 
+echo "--- git fetch"
+git fetch github
+
 echo "--- Pushing to GitHub"
 git push --force github HEAD
